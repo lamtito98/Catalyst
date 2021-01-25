9e55e8ff67ba2d483a939b259cc3075d5eb65120
Estimond Lamson Lamour <lamsonestimond2@gmail.com>
2021-01-25 15:52:49 -0500

    update
    update of the script

--- include/db_connection.php
+++ include/db_connection.php
@@ -10,15 +10,20 @@ try{
 // creates the PostgreSQL database connection
 $connection = new PDO($database_string);
 
-/* message if connected to the PostgreSQL successfully
+//message if connected to the PostgreSQL successfully
 if($connection){
-echo "Connected to the $dbname database successfully!";
-}*/
+    
+    $user_table = "CREATE TABLE IF NOT EXISTS users (
+        name character varying not null,
+        surname character varying not null,
+        email character varying  not null,
+        primary key (email))";
+
+        $connection->exec($user_table);
+    }
 }catch (PDOException $e){
 // should there be an error lets get that and show it to the user.
 echo $e->getMessage();
 }
-
-
 
 ?>
--- index.php
+++ index.php
@@ -1,7 +1,25 @@
 <?php
-include ("include/db_connection.php");
-
-if($connection){
-    echo "Connected to the $dbname database successfully!";
-    }
+require_once("include/db_connection.php");
+require_once("csv.php");
+$import_csv = new Csv();
+if(isset($_POST['sub'])){
+    var_dump($_FILES['file']);
+    $import_csv->importFile($_FILES['file']['tmp_name']);
+    //$csv1 ->im
+    
+}
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+
+<body>
+    <h1>CSV File import</h1>
+    <form method="post" enctype="multipart/form-data">
+        <input type="file" name= "file">
+        <input type="submit" name="sub" value="Import Data">
+    </form>
+</body>
+</html>
